6c927119e716ad62b473ddd9b5ec0564111a5920
Jonny Johannes <jonny@jonnyjohannes.com>
2020-11-09 08:04:50 -0500

    flash sync

--- keyboards/dz60/keymaps/jonnyjohannes/keymap.c
+++ keyboards/dz60/keymaps/jonnyjohannes/keymap.c
@@ -6,14 +6,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,
-        KC_LCTL, MO(1),   KC_LGUI,                   KC_SPC,  KC_SPC,  KC_SPC,           KC_RGUI, MO(1),   XXXXXXX, KC_RALT, KC_RCTL
+        KC_LCTL, MO(1),   KC_LGUI,                   KC_SPC,  KC_SPC,  KC_SPC,           KC_RGUI, MO(1),   XXXXXXX, KC_RCTL, KC_RALT
     ),
 
     LAYOUT(
-        _______, _______, _______, _______,  _______, _______, _______, _______, _______, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC__VOLDOWN, KC__VOLUP, XXXXXXX, KC__MUTE,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC__VOLDOWN, KC__VOLUP, XXXXXXX, KC__MUTE,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_CAPS, _______, _______, _______, KC_PGDN, _______, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, KC_PGUP, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_PLAY_PAUSE, _______, _______, _______, _______, _______
+        _______, XXXXXXX, _______, _______, _______, _______, KC_PGUP, _______, _______, _______, _______, _______, _______, XXXXXXX,
+        _______, _______, KC_MPRV,                   KC_MPLY, KC_MPLY, KC_MPLY,          KC_MNXT, _______, XXXXXXX, _______, _______
     )
 };
